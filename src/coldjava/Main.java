cbcb3e1b229bbd275e29325a8dff2ab6a0ec377b
ebolyen <ebolyen@gmail.com>
2013-04-10 20:16:21 -0700

    Update Main.java

--- src/coldjava/Main.java
+++ src/coldjava/Main.java
@@ -4,8 +4,9 @@ package coldjava;
 public class Main {
 
     public static void main(String[] args) {
-        Protocol p = new Http();
-        String result = p.doProtocol("http://www.gamefaqs.com");
-        System.out.println(result);
+        //Protocol p = new Http();
+        //String result = p.doProtocol("http://www.gamefaqs.com");
+        //System.out.println(result);
+        BrowserWindow window = new BrowserWindow();
     }
 }
