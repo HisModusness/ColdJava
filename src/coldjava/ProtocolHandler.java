e0285346802b9055326c8b3c705a8733c728912b
Liam Westby <lw322@nau.edu>
2013-04-25 15:14:56 -0700

    Added better behavior if a protocol isn't supported.
    ColdJava now ships with two protocols built in: the http protocol it used to, an a NotSupported protocol which just returns "This protocol is not supported". The ProtocolHandler, if it detects an HTTP code 501 after the CLASS request, returns an instance of this protocol instead. Additionally, the crucial line that caches the protocols was actually added because reasons.

--- src/coldjava/ProtocolHandler.java
+++ src/coldjava/ProtocolHandler.java
@@ -10,11 +10,15 @@ import java.net.URL;
 import java.util.HashMap;
 
 public class ProtocolHandler {
+
     static HashMap<String, Protocol> cache = new HashMap<String, Protocol>();
-    
+
     public ProtocolHandler() {
         if (!cache.containsKey("http")) {
             cache.put("http", new Http());
+        }
+        if (!cache.containsKey("ns")) {
+            cache.put("ns",new NotSupported());
         }
     }
 
@@ -29,55 +33,49 @@ public class ProtocolHandler {
             String protocol;
             if (Uri.indexOf(':') == Uri.length() - 1) {
                 protocol = Uri.substring(0, Uri.length() - 1);
-            }
-            else {
+            } else {
                 URI uri = new URI(Uri);
                 protocol = uri.getScheme();
             }
             if (cache.containsKey(protocol)) {
                 return cache.get(protocol);
             }
-//            if (uri.getRawSchemeSpecificPart().equals("http")) { //HTTP is already known, so it can run
-//                URL url = new URL(Uri);
-//                in = new BufferedReader(new InputStreamReader(url.openStream()));
-//
-//                String inputLine;
-//                while ((inputLine = in.readLine()) != null) {
-//                    result.append(inputLine);
-//                }
-//            } else {
-                Socket sock = new Socket("localhost", 23657);                                             //Opens the socket to the server
 
-                PrintWriter printOut = new PrintWriter(sock.getOutputStream(), true);
-                BufferedReader readIn = new BufferedReader(new InputStreamReader(sock.getInputStream()));
-                printOut.println("CLASS " + protocol + " HTTP/1.0");
-                printOut.println("");
+            Socket sock = new Socket("localhost", 23657);                                             //Opens the socket to the server
 
-                BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
-                String serverInput, userInput;
+            PrintWriter printOut = new PrintWriter(sock.getOutputStream(), true);
+            BufferedReader readIn = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+            printOut.println("CLASS " + protocol + " HTTP/1.0");
+            printOut.println("");
 
+            BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+            String serverInput, userInput;
+
+            serverInput = readIn.readLine();
+            int code = Integer.valueOf(serverInput.split(" ")[1]);
+            if (code == 501) {
+                return cache.get("ns");
+            }
+
+            while (serverInput != null) {
+                System.out.println(serverInput);
+                address = serverInput;
                 serverInput = readIn.readLine();
 
+            }
 
-                while (serverInput != null) {
-                    System.out.println(serverInput);
-                    address = serverInput;
-                    serverInput = readIn.readLine();
+            System.out.println(address);                                                              //Retrieves the address of the class file
 
-                }
+            HTTPClassLoader load = new HTTPClassLoader("localhost", 23657, "");
 
-                System.out.println(address);                                                              //Retrieves the address of the class file
+            Class protoClass = load.findClass(address);
+            testProto = (Protocol) protoClass.newInstance();
+            cache.put(protocol, testProto);
 
-                HTTPClassLoader load = new HTTPClassLoader("localhost", 23657, "");
-
-                Class protoClass = load.findClass(address);
-                testProto = (Protocol) protoClass.newInstance();
-
-                printOut.close();
-                readIn.close();
-                stdIn.close();
-                sock.close();
-            //}
+            printOut.close();
+            readIn.close();
+            stdIn.close();
+            sock.close();
 
         } catch (Exception e) {
             System.out.println("unknown protocol");
